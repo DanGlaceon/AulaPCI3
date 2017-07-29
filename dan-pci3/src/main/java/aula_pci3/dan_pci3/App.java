d78c31c8d12ae458e4f9ce25397096a9984d58a7
Aluno <danilo.pereirasilva@outlook.com>
2017-07-28 21:43:16 -0300

    teste git

---
+++ dan-pci3/src/main/java/aula_pci3/dan_pci3/App.java
@@ -0,0 +1,13 @@
+package aula_pci3.dan_pci3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Aula PCI3 28/07/2017" );
+    }
+}
